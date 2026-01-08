16d5f1cc48215ac8c51a48701584ec4c9e763d35
CheeseLord <115614743+Cheesie11@users.noreply.github.com>
2026-01-08 16:27:32 +0100

    initial commit for the frontend project (#15)
    it sets a template for the frontend project for the entire FE team to work on

---
+++ menhir-webshop/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-main class="pa-6">
+      <v-btn color="primary">Test</v-btn>
+
+      <UserView/>
+      
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import UserView from './views/UserView.vue'
+</script>
---
+++ menhir-webshop/src/views/UserView.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-main class="pa-6">
+      <Card/>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import Card from '../components/Card.vue'
+</script>
