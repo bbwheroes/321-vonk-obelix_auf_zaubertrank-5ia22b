3bef8066cd89902dc7eaf34517656d8a85ecfd32
Petr Cerny <petr.cerny@e3ag.ch>
2026-01-11 14:27:26 +0100

    feature/basket_API basket GET

--- backend/obelix-basket-api/src/main/java/ch/bbw/obelix/basket/api/BasketApi.java
+++ backend/obelix-basket-api/src/main/java/ch/bbw/obelix/basket/api/BasketApi.java
@@ -5,12 +5,16 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.service.annotation.DeleteExchange;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.PostExchange;
+import org.springframework.web.service.annotation.PutExchange;
 
 import java.util.UUID;
 
 public interface BasketApi {
 
-    @GetExchange("/api/basket/offer")
+    @GetExchange("/api/basket")
+    public BasketDto getBasket();
+
+    @PutExchange("/api/basket/offer")
     public BasketDto offer(@RequestBody BasketDto.BasketItem basketItem);
 
     @DeleteExchange("/api/basket")
