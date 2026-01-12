5569f30f5769e934f8eed13eb14ed4b721712813
Petr Cerny <petr.cerny@e3ag.ch>
2026-01-12 11:35:18 +0100

    basket_API api fix

--- backend/obelix-basket-api/src/main/java/ch/bbw/obelix/basket/api/BasketApi.java
+++ backend/obelix-basket-api/src/main/java/ch/bbw/obelix/basket/api/BasketApi.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.service.annotation.DeleteExchange;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.PostExchange;
-import org.springframework.web.service.annotation.PutExchange;
 
 import java.util.UUID;
 
@@ -14,7 +13,7 @@ public interface BasketApi {
     @GetExchange("/api/basket")
     public BasketDto getBasket();
 
-    @PutExchange("/api/basket/offer")
+    @GetExchange("/api/basket/offer")
     public BasketDto offer(@RequestBody BasketDto.BasketItem basketItem);
 
     @DeleteExchange("/api/basket")
